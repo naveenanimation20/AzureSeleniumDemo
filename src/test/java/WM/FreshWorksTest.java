bd8d11fed3f03f4ee3bcf404b71c38c060723e78
Naveen Khunteta <NaveenKhunteta@Naveen-Mac.local>
2020-07-25 15:43:28 +0530

    updated test

--- src/test/java/WM/FreshWorksTest.java
+++ src/test/java/WM/FreshWorksTest.java
@@ -2,19 +2,20 @@ package WM;
 
 import static org.testng.Assert.assertEquals;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -30,40 +31,61 @@ public class FreshWorksTest {
 	public void setup(String browser) {
 		if (browser.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
-			//driver = new ChromeDriver();
-			DesiredCapabilities cap = new DesiredCapabilities();
-			cap.setCapability("browserName", "chrome");
-			try {
-				driver = new RemoteWebDriver(new URL("http://13.233.116.37:4444/wd/hub"), cap);
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			}
+			driver = new ChromeDriver();
+			// DesiredCapabilities cap = new DesiredCapabilities();
+			// cap.setCapability("browserName", "chrome");
+			// try {
+			// driver = new RemoteWebDriver(new
+			// URL("http://13.233.116.37:4444/wd/hub"), cap);
+			// } catch (MalformedURLException e) {
+			// e.printStackTrace();
+			// }
 		}
 
 		else if (browser.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
-			//driver = new FirefoxDriver();
-			DesiredCapabilities cap = new DesiredCapabilities();
-			cap.setCapability("browserName", "firefox");
-			try {
-				driver = new RemoteWebDriver(new URL("http://13.233.116.37:4444/wd/hub"), cap);
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			}
+			driver = new FirefoxDriver();
+			// DesiredCapabilities cap = new DesiredCapabilities();
+			// cap.setCapability("browserName", "firefox");
+			// try {
+			// driver = new RemoteWebDriver(new
+			// URL("http://13.233.116.37:4444/wd/hub"), cap);
+			// } catch (MalformedURLException e) {
+			// e.printStackTrace();
+			// }
 
 		}
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://www.freshworks.com/");
 
 	}
 
-	@Test
+	@Test(priority = 1)
+	public void freshWorkslogoTest() {
+		boolean flag = false;
+		try {
+			flag = driver.findElement(By.cssSelector("a.logo.logo-fworks11")).isDisplayed();
+		} catch (Exception e) {
+			String src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE).toString();
+			File srcFile = new File(src);
+			File destFile = new File("./screenshots/logo/"+System.currentTimeMillis()+".png");
+			try {
+				FileUtils.copyFile(srcFile, destFile);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			Assert.assertEquals(flag, true);
+
+		}
+	}
+
+	@Test(priority = 2)
 	public void freshWorksTitleTest() {
 		System.out.println(driver.getTitle());
 		assertEquals(driver.getTitle(), "A fresh approach to customer engagement");
 	}
 
-	@Test
+	@Test(priority = 3)
 	public void getFooterLinksTest() {
 		List<WebElement> footerLinksList = driver.findElements(By.cssSelector("ul.footer-nav li a"));
 		footerLinksList.forEach(ele -> System.out.println(ele.getText()));
